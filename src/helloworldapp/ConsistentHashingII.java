b4c647edbb76de6c7b076d0e8edf5e1b80707adf
Sam <zhong_xun@msn.com>
2016-07-26 17:11:00 -0700

    test1

---
+++ src/helloworldapp/ConsistentHashingII.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ *
+ * @author v-xuzhon
+ */
+public class ConsistentHashingII {
+ 
+    // @param n a positive integer
+    // @param k a positive integer
+    // @return a Solution object
+    public static ConsistentHashingII create(int n, int k) {
+        // Write your code here
+    }
+
+    // @param machine_id an integer
+    // @return a list of shard ids
+    public List<Integer> addMachine(int machine_id) {
+        // Write your code here
+    }
+
+    // @param hashcode an integer
+    // @return a machine id
+    public int getMachineIdByHashCode(int hashcode) {
+        // Write your code here
+    }   
+}
